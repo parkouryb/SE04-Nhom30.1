2f0083de0b1237b8543fcbb53ce8900096fbebb2
Hahieu2k <147523689a@gmail.com>
2020-10-29 23:01:55 +0700

    update hibernate.cfg.xml

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -1,7 +1,27 @@
 package Server;
+import Hibernate.HibernateUtils;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 public class Server {
     public static void main(String[] args) {
-        System.out.println("goodbye world?");
+
+        SessionFactory factory = HibernateUtils.getSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            session.getTransaction().begin();
+            System.out.println("goodbye world?");
+
+            session.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+            session.getTransaction().rollback();
+        }
+
+        HibernateUtils.shutdown();
+
     }
 }
